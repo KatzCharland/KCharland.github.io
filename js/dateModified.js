39f5956ca03f19926b9431a6b810c62310124af9
Katherine  Charland <cha18055@byui.edu>
2020-01-18 20:16:54 -0500

    24

--- js/dateModified.js
+++ js/dateModified.js
@@ -1,16 +1,13 @@
-//alert (document.lastModified)
 function updateClock() {
-   // var now = new Date(), // current date
-      //  months = ['January', 'February', '...']; // you get the idea
-      //  time = now.getHours() + ':' + now.getMinutes()//, // again, you get the idea
-
-        // a cleaner way than string concatenation
-        date = [now.getDate(), 
-               months[now.getMonth()],
-                now.getFullYear()].join(' ');
-
-    // set the content of the element with the ID time to the formatted string
-     var elem = getElementById("currentDate"); //value = "Katz";//[date].join(' / ');
-elem.text="Katz";
    
+
+   var now = new Date(); 
+
+   var myDate = String(now.getMonth() + 1).padStart(2, '0') + '/' + now.getDate() + '/' + now.getFullYear();
+
+
+   alert(myDate);
+   document.getElementById('currentDate').innerHTML = myDate;
+  
+
 }
